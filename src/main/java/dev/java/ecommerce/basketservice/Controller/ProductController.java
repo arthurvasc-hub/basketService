31e00c79c3192d3ff121e11a573a529ba12df5eb
arthurvasc <arthurvasc2@gmail.com>
2025-04-09 17:41:50 -0300

    create endpoints to list all products or product by id from external API

--- src/main/java/dev/java/ecommerce/basketservice/Controller/ProductController.java
+++ src/main/java/dev/java/ecommerce/basketservice/Controller/ProductController.java
@@ -20,7 +20,6 @@ public class ProductController {
 
     @GetMapping
     public ResponseEntity<List<PlatziProductResponse>> getAllProducts(){
-
         return ResponseEntity.ok(service.allProducts());
     }
 
--- src/main/java/dev/java/ecommerce/basketservice/Service/ProductService.java
+++ src/main/java/dev/java/ecommerce/basketservice/Service/ProductService.java
@@ -3,22 +3,29 @@ package dev.java.ecommerce.basketservice.Service;
 import dev.java.ecommerce.basketservice.Client.PlatziStoreClient;
 import dev.java.ecommerce.basketservice.Response.PlatziProductResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final PlatziStoreClient platziStoreClient;
 
+    @Cacheable(value = "products")
     public List<PlatziProductResponse> allProducts(){
+        log.info("Getting all products");
         return platziStoreClient.getAllProducts();
     }
 
-    public PlatziProductResponse productById(Long id){
-        return platziStoreClient.getProductById(id);
+    @Cacheable(value = "products", key = "#productId")
+    public PlatziProductResponse productById(Long productId){
+        log.info("Getting product with productId: {} ", productId  );
+        return platziStoreClient.getProductById(productId);
     }
 
 }
